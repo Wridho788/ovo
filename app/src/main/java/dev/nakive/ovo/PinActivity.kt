6a9b6178e635dd3ccf63814151ba51a237108429
ridho <wridho246@gmail.com>
2020-08-08 13:15:39 +0700

    initial commit

--- app/src/main/java/dev/nakive/ovo/PinActivity.kt
+++ app/src/main/java/dev/nakive/ovo/PinActivity.kt
@@ -1,11 +1,28 @@
 package dev.nakive.ovo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.alimuzaffar.lib.pin.PinEntryEditText
 
 class PinActivity : AppCompatActivity() {
+    var pinEntry: PinEntryEditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pin)
+        pinEntry = findViewById<View>(R.id.txt_pin_entry) as PinEntryEditText
+        if (pinEntry != null) {
+            pinEntry!!.setOnPinEnteredListener { str ->
+                if (str.toString() == "1234") {
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(this, "FAIL", Toast.LENGTH_SHORT).show()
+                    pinEntry!!.text = null
+                }
+            }
+        }
     }
 }
